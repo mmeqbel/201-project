6f61c627f03e8d870e17c341bddf6b9d17961f87
motasemAlsqoor <motasem900khaled@gmail.com>
2020-12-29 21:47:03 +0200

    make the fondation

---
+++ js/app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+
+var userName=prompt("Tell me your name please ");
+alert("Welcome "+userName+" to my page ");
+var firstAnswer=prompt("is motasem meqbel my name ? [y,n]");
+//console.log(firstAnswer);
+(firstAnswer.toLowerCase()=="y") ? alert("mmm , exactly , you know my name ") : alert("mmm , however my name is motasem mrqbel ") ;
+var seconedAnswer=prompt("Am i intrested in software developing ? [y,n]");
+//console.log(seconedAnswer);
+(seconedAnswer.toLowerCase()=="y") ?alert("oops , you know so much thing about me ") : alert("there are nothing else  interst me  ");
+var thirdAnswer=prompt("Do i live jordan ? [y,n]");
+//console.log(thirdAnswer);
+(thirdAnswer.toLowerCase()=="y") ? alert("mm, ok i  was born in jordan and i have not recived any travel chance yet ") : alert("I wich your answer correct , however i live in jordan");
+var fourthAnswer=prompt("Do i like online teaching ? [y,n]");
+//console.log(fourthAnswer);
+(fourthAnswer.toLowerCase()=="y") ? alert("of course i like it , true") : alert("mmm, i like to study and work remotly , try again");
+var fifthAnswer=prompt("Do i have BA. COMPUTER SCIENCE degree ? [y,n]");
+//console.log(fifthAnswer);
+(fifthAnswer.toLowerCase()=="y") ? alert("Exactly ") : alert("mmm , i guess you  never know me");
+
+
+
+
