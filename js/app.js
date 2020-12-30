1536abecd530ba5c1214ce02b4768a68543716ef
motasemAlsqoor <motasem900khaled@gmail.com>
2020-12-31 00:02:59 +0200

    refactor

--- js/app.js
+++ js/app.js
@@ -3,26 +3,48 @@
 
 var userName=prompt("Tell me your name please ");
 alert("Welcome "+userName+" to my page ");
+
 var firstAnswer=prompt("is motasem meqbel my name ? [y,n]");
+while(firstAnswer!="y" && firstAnswer!=="n" ) {
+    firstAnswer=prompt("is motasem meqbel my name ? [y,n]");
+}
 //console.log(firstAnswer);
 (firstAnswer.toLowerCase()=="y") ? alert("mmm , exactly , you know my name ") : alert("mmm , however my name is motasem mrqbel ") ;
+
 var seconedAnswer=prompt("Am i intrested in software developing ? [y,n]");
+while(seconedAnswer!='y' && seconedAnswer!='n'  ) {
+    seconedAnswer=prompt("Am i intrested in software developing ? [y,n]");
+}
 //console.log(seconedAnswer);
+
 (seconedAnswer.toLowerCase()=="y") ?alert("oops , you know so much thing about me ") : alert("there are nothing else  interst me  ");
+
 var thirdAnswer=prompt("Do i live jordan ? [y,n]");
+while(thirdAnswer!='y' && thirdAnswer!='n'  ) {
+    thirdAnswer=prompt("Do i live jordan ? [y,n]");
+}
 //console.log(thirdAnswer);
 (thirdAnswer.toLowerCase()=="y") ? alert("mm, ok i  was born in jordan and i have not recived any travel chance yet ") : alert("I wich your answer correct , however i live in jordan");
+
 var fourthAnswer=prompt("Do i like online teaching ? [y,n]");
+while(fourthAnswer!='y' && fourthAnswer!='n'  ) {
+    fourthAnswer=prompt("Do i like online teaching ? [y,n]");
+}
 //console.log(fourthAnswer);
 (fourthAnswer.toLowerCase()=="y") ? alert("of course i like it , true") : alert("mmm, i like to study and work remotly , try again");
 var fifthAnswer=prompt("Do i have BA. COMPUTER SCIENCE degree ? [y,n]");
+while(fifthAnswer!='y' && fifthAnswer!='n'  ) {
+    fifthAnswer=prompt("Do i have BA. COMPUTER SCIENCE degree ? [y,n]");
+}
 //console.log(fifthAnswer);
 (fifthAnswer.toLowerCase()=="y") ? alert("Exactly ") : alert("mmm , i guess you  never know me");
 
+/*
 var number = 5;
 var userGuess=parseInt(prompt("guess number between 1 and 10"));
 const maxAttempt=5;
 var count=0;
+
 while(userGuess != number){
     if(count==maxAttempt+1){
         break;
@@ -79,7 +101,7 @@ while(continueGuess){
         validAnswerCount++;
     answerValid=false;
     // third question validation
-    var ans3=parseInt(prompt("write a  number less than 10 and accept division by 3 with no Remainder"));
+    var ans3=parseInt(prompt("write a primary number less than 10 and accept division by 3 with no Remainder"));
     for(var i=0;i<arr2.length;i++){//validation
           if(ans3==arr3[i]){
               answerValid=true;
@@ -96,8 +118,8 @@ while(continueGuess){
     
 }//end while
 
-alert(" great you solve "+validAnswerCount+" question correctly")
-
+alert(" great you solve "+validAnswerCount+" question correctly");
+*/
 
 
 
